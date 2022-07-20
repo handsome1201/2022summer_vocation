5314bd5b2b7c3268784360c7891294a8a0b4a5af
handsome1201 <37867002+handsome1201@users.noreply.github.com>
2022-07-20 22:03:31 +0900

    Add files via upload

---
+++ SummerVocation/src/a_0720_new1.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class a_0720_new1 {
+	static int n, m;
+	static int[] parent;
+	static int count;
+	static boolean[] visited;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		n = Integer.parseInt(br.readLine());
+		parent = new int[n];
+		String[] word = new String[n];
+		int root = 0;
+		word=br.readLine().split(" ");
+		for(int i=0;i<n;i++) {
+			parent[i]=Integer.parseInt(word[i]);
+			if(parent[i]==-1) {
+				root=i;
+			}
+		}
+
+		int  m = Integer.parseInt(br.readLine());
+
+		deleteNode(m);
+
+		count = 0;
+		visited = new boolean[n];
+		countLeaf(root);
+
+
+		System.out.println(count);
+	}
+
+	static void deleteNode(int d) {
+		parent[d]=-2;
+		for(int i=0;i<n;i++) {
+			if(parent[i]==d) {
+				deleteNode(i);
+			}
+		}
+	}
+	static void countLeaf(int s) {
+		boolean isLeaf = true;
+		visited[s] = true;
+		if(parent[s] !=-2) {
+			for(int i=0;i<n;i++) {
+				if(parent[i] == s && visited[i]==false) {
+					countLeaf(i);
+					isLeaf = false;
+				}
+			}
+			if(isLeaf) {
+				count++;
+			}
+		}
+		
+		
+	}
+}
